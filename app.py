72a07f168bd5df34d8593e47090c832a39a18a0d
pranathibestha <150576837+pranathibestha@users.noreply.github.com>
2025-07-29 21:53:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def result():
     return render_template('result.html', mood=mood, food_items=rows, background_image="bg.jpg")
 
 
-# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
